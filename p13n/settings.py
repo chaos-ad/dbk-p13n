059ed8370cc7d9dd70ea357a017c7e7f8ce66c70
Andrew Majorov <encube.ul@gmail.com>
2015-04-04 20:34:04 +0300

    App: adapt for ec2 beanstalk

--- p13n/settings.py
+++ p13n/settings.py
@@ -2,11 +2,9 @@
 
 import os
 
-os_env = os.environ
-
 
 class Config(object):
-    SECRET_KEY = os_env.get('P13N_SECRET', 'secret-key')  # TODO: Change me
+    SECRET_KEY = os.environ.get('P13N_SECRET', 'secret-key')  # TODO: Change me
     APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     BCRYPT_LOG_ROUNDS = 13
@@ -17,12 +15,16 @@ class ProdConfig(Config):
     """Production configuration."""
     ENV = 'prod'
     DEBUG = False
+    REDIS_HOST = 'customer-recs.qxlul7.ng.0001.euw1.cache.amazonaws.com'
+    REDIS_PORT = '6379'
 
 
 class DevConfig(Config):
     """Development configuration."""
     ENV = 'dev'
     DEBUG = True
+    REDIS_HOST = 'localhost'
+    REDIS_PORT = '6379'
     # Put the db file in project root
     # DB_PATH = os.path.join(Config.PROJECT_ROOT, DB_NAME)
     CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
@@ -31,4 +33,3 @@ class DevConfig(Config):
 class TestConfig(Config):
     TESTING = True
     DEBUG = True
-    BCRYPT_LOG_ROUNDS = 1  # For faster tests
